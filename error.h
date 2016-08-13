c0f41356194f62ce63b3ed54a403c24ddfe6baf4
SimGus <simon.gustin96@gmail.com>
2016-08-13 16:24:37 +0200

    Change error handling to print messages on stderr rather than stdout

--- error.h
+++ error.h
@@ -1,10 +1,10 @@
 #ifndef _ERROR_H_
 #define _ERROR_H_
 
-#define WARNING(msg);                        printf("WARNING :\n\t%s\n", msg);
-#define WARNING_FUNC(function, msg);         printf("WARNING : function '%s' (line %d)\n\t%s\n", function, __LINE__, msg);
-#define ERROR(function);                     printf("ERROR : function '%s' (line %d)\n", function, __LINE__);
-#define ERROR_MSG(function, errMsg);         printf("ERROR : function '%s' (line %d) :\n\t%s\n", function, __LINE__, errMsg);
+#define WARNING(msg);                        fprintf(stderr, "WARNING :\n\t%s\n", msg);
+#define WARNING_FUNC(function, msg);         fprintf(stderr, "WARNING : function '%s' (line %d)\n\t%s\n", function, __LINE__, msg);
+#define ERROR(function);                     fprintf(stderr, "ERROR : function '%s' (line %d)\n", function, __LINE__);
+#define ERROR_MSG(function, errMsg);         fprintf(stderr, "ERROR : function '%s' (line %d) :\n\t%s\n", function, __LINE__, errMsg);
 #define ERROR_FILE_OPENING(function, file);  char msg[256]; \
                                              sprintf(msg, "Couldn't open or create file %s", file); \
                                              ERROR_MSG(function, msg);
